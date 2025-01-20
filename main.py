f5f282125f6dac251ad81b78c68c6f7888ab083e
JohnM89 <mjammer2@gmail.com>
2025-01-20 17:13:41 -0500

    function forunique character enumeration

--- main.py
+++ main.py
@@ -5,6 +5,20 @@
 def main():
     with open("./books/frankenstein.txt") as f:
         file_contents = f.read()
-        words = len(file_contents.split())
-        print(words)
+        words = file_contents.lower()
+        print(len(words))
+        char_dict = char_duplicates(words)
+        print(char_dict)
+
+def char_duplicates(words):
+    char_count = {}
+    for letter in words:
+        if letter in char_count:
+            char_count[letter] += 1
+        else:
+            char_count[letter] = 1
+    return char_count
+
 main()
+
+
